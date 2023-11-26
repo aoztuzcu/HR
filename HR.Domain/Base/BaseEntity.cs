799147713961c5e8ffbe29afa0a37e77d6a70bed
MhmCkn <mhm.coskun@gmail.com>
2023-11-26 12:31:36 +0300

    Add project files.

---
+++ HR.Domain/Base/BaseEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Domain.Base
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool IsActive { get; set; } = true;
+        public DateTime CreateDate { get; set; }
+        public DateTime? UpdateDate { get; set; }
+        public DateTime? DeletedDate { get; set; }
+
+
+    }
+}
