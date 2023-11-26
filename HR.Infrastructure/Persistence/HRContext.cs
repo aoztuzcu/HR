c7ee57a0d51e478b13d59c40cd55d46f56982a9b
Administrator <Administrator@STUFF>
2023-11-26 14:03:10 +0300

    commit

--- HR.Infrastructure/Persistence/HRContext.cs
+++ HR.Infrastructure/Persistence/HRContext.cs
@@ -7,16 +7,6 @@ using System.Threading.Tasks;
 namespace HR.Infrastructure.Persistence
 {
     public class HRContext
-    {       
-        /**
-         * Entity oluşturulcak
-         * Viewmodel oluştulucak(Listeleme,güncelleme)
-         * Mapping class yapılıcak
-         * Command ve Query handler features lar yazılıcak. == Update, getbyid
-         * Ibase ve base repository , personrepository oluşturulucak
-         * 
-         * 
-         **/
-        //
+    {
     }
 }
