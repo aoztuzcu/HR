331f6ade10a2edc8ebaba357b4bccd521bcca6dc
Administrator <Administrator@STUFF>
2023-11-26 14:48:42 +0300

    Entity oluşturuldu

--- HR.Domain/Concrete/Job.cs
+++ HR.Domain/Concrete/Job.cs
@@ -1,12 +1,14 @@
-﻿using System;
+﻿using HR.Domain.Base;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HR.Domain.Concrete
-{
-    internal class Job
-    {
-    }
+namespace HR.Domain.Concrete;
+
+public class Job:BaseEntity
+{    public string Name { get; set; }
+    public IEnumerable<Person> Persons { get; set; }
+
 }
