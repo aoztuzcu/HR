c174f25cd1538d57ecfa9bc5c6412ea43895cd22
Administrator <Administrator@STUFF>
2023-11-26 15:19:46 +0300

    Viewmodel ve mapping eklendi

---
+++ HR.Application/Features/People/ViewModels/PersonVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HR.Application.Features.People.ViewModels;
+
+public class PersonVM
+{
+    //Özet bilgiler; fotoğraf, ad, soyad, mail, telefon, adres, meslek, departman buradaki ad soyad ikincileri de barındırıcak (en sağ üst fotoğraf) Dikdörtgen bir şekilde cardda özet bilgilerimi görmek istiyorum. 
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Mail { get; }
+    public string Address { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Photo { get; set; }
+    //public Guid JobId { get; set; }
+    //public Job Job { get; set; }
+    //public Guid DepartmentId { get; set; }
+    //public Department Department { get; set; }
+    public string DepartmentName { get; set; }
+    public string JobName { get; set; }
+}
