884c85c299e5c1c6dd3a28242a71bbe4baa8fb70
furkanumman <furkanumman@gmail.com>
2023-12-01 21:57:45 +0300

    AdvancePayment Entity Created

---
+++ HR.Domain/Concrete/AdvancePayment.cs
@@ -0,0 +1,19 @@
+﻿using HR.Domain.Base;
+using HR.Domain.Enum;
+
+namespace HR.Domain.Concrete;
+
+public class AdvancePayment : BaseEntity
+{
+    // Navigation Property
+    public Guid PersonId { get; set; }
+    public Person? Person { get; set; }
+
+    // Entity Properties
+    public AdvanceType AdvanceType { get; set; }
+    public decimal Amount { get; set; }
+    public CurrencyType CurrencyType { get; set; }
+    public ApprovalStatus ApprovalStatus { get; set; } = ApprovalStatus.ApprovalWaiting;
+    public DateTime ApprovalDate { get; set; }
+    public string Description { get; set; } = null!;
+}
