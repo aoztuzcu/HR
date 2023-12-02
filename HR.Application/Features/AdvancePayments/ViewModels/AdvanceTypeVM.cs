bfb5a1081ff7bd1df110c8ef2a71544f825ab8c2
furkanumman <furkanumman@gmail.com>
2023-12-02 13:56:44 +0300

    BaseRepository AddAsync Added

---
+++ HR.Application/Features/AdvancePayments/ViewModels/AdvanceTypeVM.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.Application.Features.AdvancePayments.ViewModels;
+
+public enum AdvanceTypeVM : byte
+{
+    [Display(Name = "Bireysel")]
+    Personal = 1,
+    [Display(Name = "Kurumsal")]
+    Corporate
+}
