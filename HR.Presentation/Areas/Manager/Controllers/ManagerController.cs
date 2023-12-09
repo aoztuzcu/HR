a54c2bed737e4b9678b23fecf138d767e8333c89
furkanumman <furkanumman@gmail.com>
2023-12-09 10:00:28 +0300

    manager area and controller added

---
+++ HR.Presentation/Areas/Manager/Controllers/ManagerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HR.Presentation.Areas.Manager.Controllers
+{
+    public class ManagerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
