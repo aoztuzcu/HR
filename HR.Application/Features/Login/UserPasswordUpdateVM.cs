638e960bad0358a8f7daaa8b4f017ec69ae3f996
furkanumman <furkanumman@gmail.com>
2023-12-11 15:35:26 +0300

    user new password setting completed

---
+++ HR.Application/Features/Login/UserPasswordUpdateVM.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HR.Application.Features.Login;
+
+public class UserPasswordUpdateVM
+{
+    [Required(ErrorMessage = "Mevcut Şifre boş bırakılamaz.")]
+    public string CurrentPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "Yeni Şifre boş bırakılamaz.")]
+    [StringLength(100, ErrorMessage = "Şifre en az {2} karakter uzunluğunda olmalıdır.", MinimumLength = 6)]
+    public string NewPassword { get; set; } = null!;
+
+    [Required(ErrorMessage = "Yeni Şifre Tekrarı boş bırakılamaz.")]
+    [Compare("NewPassword", ErrorMessage = "Yeni şifre ile onay şifresi eşleşmiyor.")]
+    public string NewPasswordConfirm { get; set; } = null!;
+}
