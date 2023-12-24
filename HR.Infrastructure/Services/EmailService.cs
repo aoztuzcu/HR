90777cedb4b99ecc161a93afa4e85a9ad2628ec6
Administrator <Administrator@STUFF>
2023-12-24 16:44:18 +0300

    Companylist and mail edit

--- HR.Infrastructure/Services/EmailService.cs
+++ HR.Infrastructure/Services/EmailService.cs
@@ -37,4 +37,31 @@ public class EmailService : IEmailService
 
     }
 
+    public async Task SendConfirmMailByAdmin(string mailAddress, string sendPassword, string adminInfo)
+    {
+        MimeMessage mimeMessage = new MimeMessage();
+        MailboxAddress mailboxAddressFrom = new MailboxAddress("IKIZLEME MANAGER", "ikizlemeproject@gmail.com");
+        MailboxAddress mailboxAddressTo = new MailboxAddress("User", mailAddress);
+
+        mimeMessage.From.Add(mailboxAddressFrom);
+        mimeMessage.To.Add(mailboxAddressTo);
+
+        var bodyBuilder = new BodyBuilder();
+        bodyBuilder.TextBody = $"Site Yöneticiniz {adminInfo} tarafından kaydınız yapılmıştır. Sisteme giriş yapabilmeniz için gerekli bilgiler aşağıda verilmiştir.\n" +
+               $"E - Mail: {mailAddress}\n" +
+                  $"Password: {sendPassword}\n" +
+                     "\n" +
+                     "URL: https://ikizlemehrr.azurewebsites.net/";
+
+        mimeMessage.Body = bodyBuilder.ToMessageBody();
+        mimeMessage.Subject = "Yeni Üye Kayıt Tamamlama";
+
+        using (var smtpClient = new SmtpClient())
+        {
+            await smtpClient.ConnectAsync("smtp.gmail.com", 587, false);
+            await smtpClient.AuthenticateAsync("ikizlemeproject@gmail.com", "crokwoumhkwvlphm");
+            await smtpClient.SendAsync(mimeMessage);
+            await smtpClient.DisconnectAsync(true);
+        }
+    }
 }
