e851cf2abc7310d2ab13df883ec08f74fed6e096
Abdulkadir Oztuzcu <128609009+aoztuzcu@users.noreply.github.com>
2024-02-07 10:22:44 +0300

    inital

---
+++ HR.Presentation/obj/Release/net6.0/PubTmp/Out/wwwroot/js/MinDateTime.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+    // Sayfa yüklendiğinde çalışacak JavaScript kodları buraya yazılabilir
+    setMinStartDate();
+});
+
+function setMinStartDate() {
+    // Bugünkü tarihi al
+    var today = new Date();
+
+    // Yarını hesapla
+    var tomorrow = new Date(today);
+    tomorrow.setDate(today.getDate() + 1);
+
+    // Yarından sonraki tarihi formatla (gerekirse)
+    var formattedTomorrow = formatDate(tomorrow);
+
+    // Başlangıç tarih alanını seç ve en erken tarihi ayarla
+    $("#StartDate").val(formattedTomorrow);
+}
+
+function formatDate(date) {
+    // Tarihi "yyyy-MM-dd" formatına çevir
+    var dd = String(date.getDate()).padStart(2, '0');
+    var mm = String(date.getMonth() + 1).padStart(2, '0'); // Ocak 0, Şubat 1, vs.
+    var yyyy = date.getFullYear();
+
+    return yyyy + '-' + mm + '-' + dd;
+}
